7475c7a79526131ed3f25907a80b5a5cb4fb8364
redbackspider <66238571+redbackspider69@users.noreply.github.com>
2025-06-06 12:35:20 +1000

    Create Wellio Locked Question Unblurrer-2025-06-06.user.js

---
+++ Wellio Locked Question Unblurrer-2025-06-06.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Wellio Locked Question Unblurrer
+// @namespace    http://tampermonkey.net/
+// @version      2025-06-06
+// @description  Userscript which unblurs all locked questions in a Wellio quiz, so one may do them before their peasantly friends are able to. However, one will only be able to submit them successfully once they are unlocked.
+// @author       redbackspider69 on Github
+// @match        https://app.wellioeducation.com/schools/*/classes/*/lessons/*/learn
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=wellioeducation.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    window.addEventListener('DOMContentLoaded', function() {
+        var blurs = Array.from(document.getElementsByClassName('bg-blur'));
+        blurs.forEach(function(blur) {
+            blur.remove();
+        });
+    }, false);
+})();
